b5c6fb1a6f23cf7587befff38424d51977ee1b5a
changhyun <cg072@naver.com>
2024-07-31 17:11:51 +0900

    Feat - MainActivity에 BaseActivity 적용
    1. BaseActivity 적용
    2. 사용하지 않는 코드 제거

--- app/src/main/java/com/goody/dalda/MainActivity.kt
+++ app/src/main/java/com/goody/dalda/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.goody.dalda
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.LayoutInflater
+import com.goody.dalda.base.BaseActivity
+import com.goody.dalda.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
+        get() = ActivityMainBinding::inflate
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
