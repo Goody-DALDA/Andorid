6c6918a6b6c3295343478e9155afa33b1377989d
changhyun <cg072@naver.com>
2024-07-31 17:10:44 +0900

    Feat - BaseActivity, BaseFragment 추가
    1. ViewBinding 추가 및 제거 코드가 포함된 BaseActivity, BaseFragment 추가

---
+++ app/src/main/java/com/goody/dalda/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.goody.dalda.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+    protected abstract val bindingInflater: (LayoutInflater) -> VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater.invoke(layoutInflater)
+        setContentView(binding.root)
+    }
+}
