bfbd9b547d599280a30a4d835f982a9f234b6461
OYJ <oyj7677@gmail.com>
2024-09-22 00:54:53 +0900

    Refactor : 웰컴 베너 리펙터링
    1. AutoResizedText 적용
    2. Row 컨텐츠 비율 조정
    3. 유저 이미지 padding(vertical = 10.dp) 추가

--- app/src/main/java/com/goody/dalda/ui/home/component/WelcomeBanner.kt
+++ app/src/main/java/com/goody/dalda/ui/home/component/WelcomeBanner.kt
@@ -3,8 +3,8 @@ package com.goody.dalda.ui.home.component
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,36 +14,38 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.goody.dalda.R
+import com.goody.dalda.ui.component.AutoResizedText
 
 @Composable
 fun WelcomeBanner(userName: String, modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         WelcomeComment(
             userName = userName,
             modifier = Modifier
-                .align(alignment = Alignment.CenterVertically)
-                .weight(5f)
+                .weight(224f)
         )
 
-        Spacer(modifier = Modifier.weight(1f))
+        Spacer(
+            modifier = Modifier
+                .weight(26f)
+        )
 
         AsyncImage(
             model = "",
             contentDescription = stringResource(id = R.string.description_user_profile_img),
             placeholder = ColorPainter(Color.Blue),
             modifier = Modifier
-                .height(70.dp)
-                .weight(1f)
-                .align(alignment = Alignment.CenterVertically)
+                .weight(70f)
+                .padding(vertical = 10.dp)
         )
     }
 }
@@ -63,12 +65,11 @@ private fun WelcomeComment(userName: String, modifier: Modifier = Modifier) {
         }
 
         append(welcomeMessage.substring(endIndex))
-    }
+    }.toString()
 
-    Text(
+    AutoResizedText(
         text = annotatedString,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Start,
+        style = MaterialTheme.typography.titleLarge,
         modifier = modifier
     )
 }
