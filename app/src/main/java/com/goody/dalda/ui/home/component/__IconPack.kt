bf75859c513f851e1822b59b2a28b91052d9eedd
OYJ <oyj7677@gmail.com>
2024-09-29 15:51:19 +0900

    Chore : 아이콘 SVG 파일 추가
    1. 카메라, 인스타, 링크, 메뉴 닷

---
+++ app/src/main/java/com/goody/dalda/ui/home/component/__IconPack.kt
@@ -0,0 +1,21 @@
+package com.goody.dalda.ui.home.component
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.goody.dalda.ui.home.component.iconpack.IcCamera
+import com.goody.dalda.ui.home.component.iconpack.IcInsta
+import com.goody.dalda.ui.home.component.iconpack.IcLink
+import com.goody.dalda.ui.home.component.iconpack.IcMenuDot
+import kotlin.collections.List as ____KtList
+
+public object IconPack
+
+private var __AllIcons: ____KtList<ImageVector>? = null
+
+public val IconPack.AllIcons: ____KtList<ImageVector>
+  get() {
+    if (__AllIcons != null) {
+      return __AllIcons!!
+    }
+    __AllIcons= listOf(IcInsta, IcLink, IcMenuDot, IcCamera)
+    return __AllIcons!!
+  }
---
+++ app/src/main/java/com/goody/dalda/ui/home/component/iconpack/IcMenuDot.kt
@@ -0,0 +1,53 @@
+package com.goody.dalda.ui.home.component.iconpack
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import com.goody.dalda.ui.home.component.IconPack
+
+public val IconPack.IcMenuDot: ImageVector
+    get() {
+        if (_icMenuDot != null) {
+            return _icMenuDot!!
+        }
+        _icMenuDot = Builder(name = "IcMenuDot", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+                viewportWidth = 24.0f, viewportHeight = 24.0f).apply {
+            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(12.0f, 5.0f)
+                moveToRelative(-2.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, 4.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, -4.0f, 0.0f)
+            }
+            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(12.0f, 12.0f)
+                moveToRelative(-2.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, 4.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, -4.0f, 0.0f)
+            }
+            path(fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(12.0f, 19.0f)
+                moveToRelative(-2.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, 4.0f, 0.0f)
+                arcToRelative(2.0f, 2.0f, 0.0f, true, true, -4.0f, 0.0f)
+            }
+        }
+        .build()
+        return _icMenuDot!!
+    }
+
+private var _icMenuDot: ImageVector? = null
