a16e00c52c7b5ad51eb5248fd200b470e3f5168f
OYJ <oyj7677@gmail.com>
2024-10-31 15:49:10 +0900

    Feat : 최근 검색어 및 유사 검색어 UI 구현
    1. 최근 검색어 UI 구현
    2. 검색어 여부에 따라 최근 검색어 or 유사 검색어 노출

--- app/src/main/java/com/goody/dalda/ui/home/component/SearchBar.kt
+++ app/src/main/java/com/goody/dalda/ui/home/component/SearchBar.kt
@@ -1,14 +1,22 @@
 package com.goody.dalda.ui.home.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,8 +37,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.goody.dalda.R
 import com.goody.dalda.ui.home.component.iconpack.IcCamera
 
@@ -39,10 +49,12 @@ import com.goody.dalda.ui.home.component.iconpack.IcCamera
 fun AlcoholSearchBar(
     modifier: Modifier = Modifier,
     query: String,
+    searchResultList: List<String> = emptyList(),
+    recentSearchWordList: List<String> = emptyList(),
     expanded: Boolean,
     onQueryChange: (String) -> Unit,
     onExpandedChange: (Boolean) -> Unit,
-    onSearch: (String) -> Unit
+    onSearch: (String) -> Unit = {}
 ) {
     Box(
         modifier = modifier
@@ -74,29 +86,94 @@ fun AlcoholSearchBar(
             expanded = expanded,
             onExpandedChange = onExpandedChange,
         ) {
-            Column(
-                Modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                repeat(4) { idx ->
-                    val resultText = "Suggestion $idx"
+            if (query.isEmpty()) {
+                Column(
+                    modifier = Modifier.padding(24.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = stringResource(R.string.text_recent_search_work)
+                        )
+                        Icon(Icons.Outlined.Clear, contentDescription = null)
+                    }
 
-                    ListItem(
-                        headlineContent = { Text(resultText) },
-                        modifier = Modifier
-                            .clickable {
-                                onQueryChange(resultText)
-                                onExpandedChange(false)
-                            }
-                            .padding(horizontal = 16.dp, vertical = 4.dp),
-                        supportingContent = { Text("Additional info") },
-                        leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
-                        colors = ListItemDefaults.colors(containerColor = Color.Transparent)
+                    AlcoholChipGrid(
+                        recentSearchWordList = recentSearchWordList,
+                        onClickWord = onQueryChange
                     )
+                }
+            } else {
+                Column(
+                    Modifier
+                        .weight(1f)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    for (searchResult in searchResultList) {
+                        ListItem(
+                            headlineContent = { Text(searchResult) },
+                            modifier = Modifier
+                                .clickable {
+                                    onQueryChange(searchResult)
+                                    onExpandedChange(false)
+                                }
+                                .padding(horizontal = 16.dp, vertical = 4.dp),
+                            supportingContent = { Text("Additional info") },
+                            leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+                            colors = ListItemDefaults.colors(containerColor = Color.Transparent)
+                        )
+                    }
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun AlcoholChipGrid(
+    modifier: Modifier = Modifier,
+    recentSearchWordList: List<String>,
+    onClickWord: (String) -> Unit = {}
+) {
+    FlowRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        recentSearchWordList.forEach { item ->
+            AlcoholChip(
+                text = item,
+                onClickWord = onClickWord
+            )
+        }
+    }
+}
+
+@Composable
+fun AlcoholChip(
+    text: String,
+    onClickWord: (String) -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .background(
+                Color(0xFFF5F5F5),
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clickable { onClickWord(text) },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
@@ -106,10 +183,14 @@ fun AlcoholSearchBar(
 private fun SearchBarPreview() {
     var text by rememberSaveable { mutableStateOf("") }
     var expanded by rememberSaveable { mutableStateOf(false) }
+    val recentSearchWordList = listOf("소주", "맥주", "막걸리")
+    val searchResultList = listOf("테스트1", "테스트2", "테스트3")
 
     AlcoholSearchBar(
         modifier = Modifier,
         query = text,
+        searchResultList = searchResultList,
+        recentSearchWordList = recentSearchWordList,
         expanded = expanded,
         onQueryChange = { text = it },
         onExpandedChange = { expanded = it },
