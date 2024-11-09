106fbf6fb442e04d63bdc0c142a93a85f702e1c8
OYJ <oyj7677@gmail.com>
2024-11-09 17:42:41 +0900

    Refactor : 상세 정보 주류 간단 정보 컴포넌트 리팩터링
    1. 하단 정보(태그, 이름, 나라, 주조장, 도수, 용량) 컴포넌트화
    2. 주조장 정보가 있는 경우 주조장 노출, 없을 경우 나라 노출
    3. 용량, 도수 없을 경우 공백

--- app/src/main/java/com/goody/dalda/ui/liquor_details/component/LiquorInfoSection.kt
+++ app/src/main/java/com/goody/dalda/ui/liquor_details/component/LiquorInfoSection.kt
@@ -1,10 +1,12 @@
 package com.goody.dalda.ui.liquor_details.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
@@ -16,10 +18,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.goody.dalda.R
+import com.goody.dalda.data.AlcoholData
 
 @Composable
 fun LiquorInfoSection(
     modifier: Modifier = Modifier,
+    alcoholData: AlcoholData
 ) {
     Column(
         modifier = modifier.padding(horizontal = 20.dp),
@@ -33,41 +37,127 @@ fun LiquorInfoSection(
             modifier = Modifier.height(200.dp),
             placeholder = painterResource(R.drawable.ic_launcher_foreground),
         )
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // 카테고리
-            Text("소주")
 
-            // 이름
-            Text("대선")
-
-            // 주소장
-            Text("대선 주조")
-        }
-
-        // 도수 / 용량
-        Row(
-            modifier = Modifier.padding(bottom = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            SecondFloorText(
-                topText = "도수",
-                bottomText = "20%"
-            )
-
-            VerticalDivider(
-                modifier = Modifier.height(40.dp)
-            )
-            SecondFloorText(
-                topText = "용량",
-                bottomText = "360ml"
-            )
-        }
-
+        BasicInfo(
+            alcoholData = alcoholData
+        )
     }
 }
+
+@Composable
+fun BasicInfo(
+    modifier: Modifier = Modifier,
+    alcoholData: AlcoholData
+) {
+    when (alcoholData) {
+        is AlcoholData.Soju,
+        is AlcoholData.Beer,
+        is AlcoholData.Sake,
+        is AlcoholData.Whiskey -> {
+            BasicInfoWithCountry(
+                alcoholData = alcoholData
+            )
+        }
+
+        is AlcoholData.TraditionalLiquor,
+        is AlcoholData.Wine -> {
+            BasicInfoWithBrewery(
+                alcoholData = alcoholData
+            )
+        }
+    }
+}
+
+@Composable
+fun BasicInfoWithCountry(
+    modifier: Modifier = Modifier,
+    alcoholData: AlcoholData
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // 카테고리
+        Image(
+            modifier = Modifier.width(32.dp),
+            painter = painterResource(alcoholData.tag),
+            contentDescription = "카테고리",
+            contentScale = ContentScale.FillWidth,
+        )
+
+        // 이름
+        Text(text = alcoholData.name)
+
+        // 나라
+        Text(alcoholData.country)
+    }
+
+    // 도수 / 용량
+    RowAbvVolume(
+        alcoholData = alcoholData
+    )
+}
+
+@Composable
+fun BasicInfoWithBrewery(
+    modifier: Modifier = Modifier,
+    alcoholData: AlcoholData
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // 카테고리
+        Image(
+            modifier = Modifier.width(32.dp),
+            painter = painterResource(alcoholData.tag),
+            contentDescription = "카테고리",
+            contentScale = ContentScale.FillWidth,
+        )
+
+        // 이름
+        Text(text = alcoholData.name)
+
+        // 양조장
+        if (alcoholData is AlcoholData.TraditionalLiquor) {
+            Text(alcoholData.brewery)
+        }
+        if (alcoholData is AlcoholData.Wine) {
+            Text(alcoholData.brewery)
+        }
+    }
+
+    // 도수 / 용량
+    RowAbvVolume(
+        alcoholData = alcoholData
+    )
+}
+
+@Composable
+fun RowAbvVolume(
+    modifier: Modifier = Modifier,
+    alcoholData: AlcoholData
+) {
+    Row(
+        modifier = Modifier.padding(bottom = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SecondFloorText(
+            topText = "도수",
+            bottomText = if (alcoholData.abv != 0f) alcoholData.abv.toString() + "%" else ""
+        )
+
+        VerticalDivider(
+            modifier = Modifier.height(40.dp)
+        )
+        SecondFloorText(
+            topText = "용량",
+            bottomText = if (alcoholData.volume != 0) alcoholData.volume.toString() + "ml" else ""
+        )
+    }
+}
+
 
 @Composable
 fun SecondFloorText(
@@ -84,10 +174,60 @@ fun SecondFloorText(
     }
 }
 
-@Preview(
-    showBackground = true
-)
+@Preview(showBackground = true)
 @Composable
-private fun LiquorImageSectionPrev() {
-    LiquorInfoSection()
+private fun LiquorImageSectionPrev_soju() {
+    LiquorInfoSection(
+        alcoholData = AlcoholData.Soju(
+            id = 7959,
+            name = "대선",
+            imgUrl = "http://www.bing.com/search?q=sagittis",
+            tag = R.drawable.tag_soju,
+            volume = 100,
+            price = 2900,
+            abv = 18f,
+            comment = "소주임"
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LiquorImageSectionPrev_beer() {
+    LiquorInfoSection(
+        alcoholData = AlcoholData.Beer(
+            id = 7959,
+            name = "1664 블랑",
+            imgUrl = "http://www.bing.com/search?q=sagittis",
+            tag = R.drawable.tag_beer,
+            abv = 18f,
+            appearance = 18f,
+            taste = 18f,
+            mouthfeel = 18f,
+            aroma = 18f,
+            country = "독일",
+            type = "밀맥주",
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LiquorImageSectionPrev_sake() {
+    LiquorInfoSection(
+        alcoholData = AlcoholData.Sake(
+            id = 7959,
+            name = "닷사이 준마이 다이긴죠 23",
+            imgUrl = "http://www.bing.com/search?q=sagittis",
+            tag = R.drawable.tag_sake,
+            volume = 360,
+            price = 200000,
+            abv = 18f,
+            taste = "달콤함",
+            aroma = "향기",
+            finish = "맛",
+            type = "사케",
+            country = "일본"
+        )
+    )
 }
