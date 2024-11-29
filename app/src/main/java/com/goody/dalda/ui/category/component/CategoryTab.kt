a69f8a5a02a95d7f516ecea6d9fde632db41a0c5
OYJ <oyj7677@gmail.com>
2024-11-29 15:51:44 +0900

    Feat : 카테고리 탭 컴포넌트 구현
    1. ScrollableTabRow를 활용하여 구현
    2. 선택되지 않은 항목 회색으로 변경

---
+++ app/src/main/java/com/goody/dalda/ui/category/component/CategoryTab.kt
@@ -0,0 +1,59 @@
+package com.goody.dalda.ui.category.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.goody.dalda.data.AlcoholType
+
+@Composable
+fun CategoryTab(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    category: List<String> = emptyList(),
+    onClickTab: (Int) -> Unit = {}
+) {
+    ScrollableTabRow(
+        modifier = modifier.fillMaxWidth(),
+        containerColor = Color.White,
+        contentColor = Color.Black,
+        selectedTabIndex = pagerState.currentPage,
+        indicator = { tabPositions ->
+            TabRowDefaults.SecondaryIndicator(
+                modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
+                color = Color.Black
+            )
+        },
+        edgePadding = 0.dp
+    ) {
+        category.forEachIndexed { index, title ->
+            Tab(
+                text = { Text(text = title) },
+                selected = pagerState.currentPage == index,
+                onClick = { onClickTab(index) },
+                unselectedContentColor = Color.Gray
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CategoryTabPrev() {
+    val category = AlcoholType.entries.map { it.alcoholName }
+    val pagerState = rememberPagerState(pageCount = { category.size })
+
+    CategoryTab(
+        pagerState = pagerState,
+        category = category
+    )
+}
