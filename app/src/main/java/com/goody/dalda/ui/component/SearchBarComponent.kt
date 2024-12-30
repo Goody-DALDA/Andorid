724d2f7f0d70f05e824e2ad35fdd29eedf12fbcb
OYJ <oyj7677@gmail.com>
2024-12-30 14:40:33 +0900

    Refactor : 검색 바 UI 수정
    1. 꼬리 아이콘 위치 고정

--- app/src/main/java/com/goody/dalda/ui/component/SearchBarComponent.kt
+++ app/src/main/java/com/goody/dalda/ui/component/SearchBarComponent.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
@@ -15,14 +17,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction.Companion.Search
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.goody.dalda.ui.home.component.IconPack
 import com.goody.dalda.ui.home.component.iconpack.IcCamera
+import kotlinx.coroutines.delay
 
 @Composable
 fun SearchBarComponent(
@@ -36,6 +46,14 @@ fun SearchBarComponent(
     onClickLeadingIcon: (String) -> Unit = {},
     onClickTrailingIcon: () -> Unit = {}
 ) {
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        delay(300)
+        focusRequester.requestFocus()
+    }
+
     Row(
         modifier = modifier,
     ) {
@@ -56,7 +74,8 @@ fun SearchBarComponent(
                 .background(
                     color = Color(0xFFF8F8F8),
                     shape = RoundedCornerShape(18.dp)
-                ),
+                )
+                .focusRequester(focusRequester),
             placeholder = { Text(text = placeholder) },
             leadingIcon = {
                 Icon(
@@ -67,33 +86,39 @@ fun SearchBarComponent(
             },
             trailingIcon = {
                 Row(
+                    modifier = Modifier.padding(end = 8.dp),
                     horizontalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
-                    if (query.isNotEmpty()) {
-                        Icon(
-                            imageVector = Icons.Outlined.Clear,
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clickable {
                                 onValueChange("")
                             }
-                        )
-                    }
+                            .alpha(
+                                if (query.isNotEmpty()) 1f else 0f
+                            )
+                    )
+
                     Icon(
                         imageVector = trailingIcon,
                         contentDescription = null,
                         modifier = Modifier.clickable {
                             onClickTrailingIcon()
-                        }
-                    )
+                        },
+
+                        )
                 }
             },
             shape = RoundedCornerShape(18.dp),
             keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = androidx.compose.ui.text.input.ImeAction.Search
+                imeAction = Search
             ),
-            keyboardActions = androidx.compose.foundation.text.KeyboardActions(
+            keyboardActions = KeyboardActions(
                 onSearch = {
                     onClickLeadingIcon(query)
+                    focusManager.clearFocus()
                 }
             )
         )
