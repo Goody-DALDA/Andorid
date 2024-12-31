0ba2be6d69e78219101d83963c1f5358045d806f
OYJ <oyj7677@gmail.com>
2024-12-31 16:10:40 +0900

    Chore : kotlin-serialization 라이브러리 추가 및 라이브러리 버전 업데이트
    1. kotlin-serialization 라이브러리 추가
    2. kotlin, hilt, ksp 라이브러리 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 buildscript {
@@ -12,5 +13,6 @@ buildscript {
     }
     dependencies {
         classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+        classpath(libs.kotlin.serialization)
     }
 }
