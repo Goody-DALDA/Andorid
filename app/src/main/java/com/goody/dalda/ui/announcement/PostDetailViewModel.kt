181d61bad3d2048f1732fc008f3bf0e073f2fa15
OYJ <oyj7677@gmail.com>
2025-02-07 13:13:12 +0900

    Refactor : Preview를 위한 PostDeetailScreen 수정
    1. stateLess를 위한 상태 호이스팅 진행
    2. StateFlow활용을 위한 PostDtailViewModel 데이터 홀더 변경
    3. 백그라운드 색상 변경 (White)

--- app/src/main/java/com/goody/dalda/ui/announcement/PostDetailViewModel.kt
+++ app/src/main/java/com/goody/dalda/ui/announcement/PostDetailViewModel.kt
@@ -1,69 +1,72 @@
 package com.goody.dalda.ui.announcement
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.goody.dalda.data.repository.NoticeRepository
 import com.goody.dalda.ui.model.Post
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PostDetailViewModel
-@Inject
-constructor(private val repository: NoticeRepository) :
-    ViewModel() {
-    private val _postState = mutableStateOf<Post?>(null)
-    val postState: State<Post?> get() = _postState
-    private val nextPostState = mutableStateOf<Post?>(null)
-    private val prevPostState = mutableStateOf<Post?>(null)
-    private val posts = mutableListOf<Post>()
-    private var postIndex: Int = 0
+    @Inject
+    constructor(
+        private val repository: NoticeRepository,
+    ) : ViewModel() {
+        private val _currentPost = MutableStateFlow<Post?>(null)
+        val currentPost: StateFlow<Post?> = _currentPost
 
-    fun fetchNoticePost(post: Post) {
-        viewModelScope.launch(Dispatchers.IO) {
-            posts.addAll(repository.fetchNotice())
+        private val _nextPost = MutableStateFlow<Post?>(null)
+        val nextPost: StateFlow<Post?> = _nextPost
+
+        private val _prevPost = MutableStateFlow<Post?>(null)
+        val prevPost: StateFlow<Post?> = _prevPost
+
+        private var postList: List<Post> = emptyList()
+
+        private var currentIndex = 0
+
+        fun fetchNoticePost(post: Post) {
+            viewModelScope.launch(Dispatchers.IO) {
+                postList = repository.fetchNotice()
+                setPost(post)
+                setNextPost()
+                setPrevPost()
+            }
+        }
+
+        private fun setPost(post: Post) {
+            _currentPost.value = post
+            currentIndex = postList.indexOf(post)
+        }
+
+        fun nextPost() {
+            if (currentIndex >= postList.size - 1) return
+            val post = postList[currentIndex + 1]
             setPost(post)
             setNextPost()
             setPrevPost()
         }
+
+        fun prevPost() {
+            if (currentIndex <= 0) return
+
+            val post = postList[currentIndex - 1]
+            setPost(post)
+            setNextPost()
+            setPrevPost()
+        }
+
+        private fun setNextPost() {
+            _nextPost.value =
+                if (currentIndex >= postList.size - 1) null else postList[currentIndex + 1]
+        }
+
+        private fun setPrevPost() {
+            _prevPost.value = if (currentIndex <= 0) null else postList[currentIndex - 1]
+        }
     }
-
-    private fun setPost(post: Post) {
-        _postState.value = post
-        postIndex = posts.indexOf(post)
-    }
-
-    fun nextPost() {
-        if (postIndex >= posts.size) return
-
-        val post = posts[postIndex + 1]
-        setPost(post)
-        setNextPost()
-        setPrevPost()
-    }
-
-    fun prevPost() {
-        if (postIndex <= 0) return
-
-        val post = posts[postIndex - 1]
-        setPost(post)
-        setNextPost()
-        setPrevPost()
-    }
-
-    fun getNextPost() = nextPostState.value
-
-    fun getPrevPost() = prevPostState.value
-
-    private fun setNextPost() {
-        nextPostState.value = if (postIndex >= posts.size - 1) null else posts[postIndex + 1]
-    }
-
-    private fun setPrevPost() {
-        prevPostState.value = if (postIndex <= 0) null else posts[postIndex - 1]
-    }
-}
