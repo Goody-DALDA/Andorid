f4906daa99874435e086c98b73827c54e345c452
OYJ <oyj7677@gmail.com>
2025-02-07 11:44:43 +0900

    Refactor : Preview 사용을 위한 상태 호이스팅
    1. post데이터 stateFlow로 데이터 홀더 변경
    2. staeless를 위한 컴포넌트 분리

--- app/src/main/java/com/goody/dalda/ui/announcement/AnnouncementScreen.kt
+++ app/src/main/java/com/goody/dalda/ui/announcement/AnnouncementScreen.kt
@@ -1,5 +1,6 @@
 package com.goody.dalda.ui.announcement
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,24 +8,91 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.goody.dalda.ui.model.Post
 
 @Composable
 fun AnnouncementScreen(
     onClick: (Post) -> Unit,
+    onClickBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: AnnouncementViewModel = viewModel(),
 ) {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(viewModel.getNoticePosts()) { item ->
-            NoticePost(item.title, item.updatedAt, onClick = { onClick(item) })
+    val posts by viewModel.posts.collectAsStateWithLifecycle()
+
+    AnnouncementScreen(
+        postList = posts,
+        onclickPost = onClick,
+        onClickBack = onClickBack,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AnnouncementScreen(
+    postList: List<Post>,
+    onclickPost: (Post) -> Unit = {},
+    onClickBack: () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.onBackground),
+        containerColor = Color.White,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "공지사항") },
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.White,
+                        titleContentColor = Color.Black,
+                    ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = onClickBack,
+                    ) {
+                        Icon(
+                            Icons.Filled.Close,
+                            contentDescription = "close",
+                        )
+                    }
+                },
+            )
+        },
+    ) { innerPadding ->
+        LazyColumn(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+        ) {
+            items(postList) { item ->
+                NoticePost(
+                    title = item.title,
+                    date = item.updatedAt,
+                    onClick = { onclickPost(item) },
+                )
+            }
         }
     }
 }
@@ -55,12 +123,34 @@ fun NoticePost(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun AnnouncementScreenPreview() {
+    val postList =
+        listOf(
+            Post(
+                id = 1,
+                title = "title1",
+                content = "content1",
+                createdAt = "2021-09-01",
+                updatedAt = "2021-09-01",
+                isActive = true,
+            ),
+            Post(
+                id = 2,
+                title = "title2",
+                content = "content2",
+                createdAt = "2021-09-02",
+                updatedAt = "2021-09-02",
+                isActive = true,
+            ),
+        )
     MaterialTheme {
         AnnouncementScreen(
-            onClick = {},
+            postList = postList,
+            onclickPost = {},
+            onClickBack = {},
+            modifier = Modifier,
         )
     }
 }
--- app/src/main/java/com/goody/dalda/ui/announcement/AnnouncementViewModel.kt
+++ app/src/main/java/com/goody/dalda/ui/announcement/AnnouncementViewModel.kt
@@ -7,6 +7,8 @@ import com.goody.dalda.data.repository.NoticeRepository
 import com.goody.dalda.ui.model.Post
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,7 +17,8 @@ class AnnouncementViewModel
 @Inject
 constructor(private val repository: NoticeRepository) :
     ViewModel() {
-    private val posts = mutableStateListOf<Post>()
+    private val _posts = MutableStateFlow<List<Post>>(emptyList())
+    val posts : StateFlow<List<Post>> = _posts
 
     init {
         fetchNoticePost()
@@ -23,9 +26,7 @@ constructor(private val repository: NoticeRepository) :
 
     private fun fetchNoticePost() {
         viewModelScope.launch(Dispatchers.IO) {
-            posts.addAll(repository.fetchNotice())
+            _posts.value = repository.fetchNotice()
         }
     }
-
-    fun getNoticePosts() = posts
 }
