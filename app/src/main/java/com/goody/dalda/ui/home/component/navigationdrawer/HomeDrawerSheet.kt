1f9845e25b6fcebc86757f5d9920f90428c63b29
OYJ <oyj7677@gmail.com>
2025-03-14 14:34:36 +0900

    Style : 사이드 메뉴 UI 개선
    1. 모서리 직각으로 변경
    2. 흰색으로 백그라운드 색상 변경

--- app/src/main/java/com/goody/dalda/ui/home/component/navigationdrawer/HomeDrawerSheet.kt
+++ app/src/main/java/com/goody/dalda/ui/home/component/navigationdrawer/HomeDrawerSheet.kt
@@ -9,6 +9,10 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,9 +43,10 @@ fun HomeDrawerSheet(
         )
 
     ModalDrawerSheet(
-        modifier =
-        modifier
+        modifier = modifier
             .fillMaxHeight(),
+        drawerShape = RectangleShape,
+        drawerContainerColor = colorResource(id = R.color.white),
     ) {
         NavigationHeader(
             modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 24.dp),
